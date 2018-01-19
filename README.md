235e22da6aea51a5e72ce7f1a443a154b97d40b3
Joaquin Santiago <JRuiz912@west-mec.org>
2018-01-18 18:09:40 -0700

    Added
    Today I added a new repository with 10 different files. Inside both
    html and the js

---
+++ README.md
@@ -0,0 +1,2 @@
+# S2_Exercise_07_02
+JavaScript Exercise 07_02: Fan Trick Photography
---
+++ ft.js
@@ -0,0 +1,10 @@
+/*
+
+        * JavaScript: Exercise 07_02
+        * Fan Trick Fine Art Photography
+        * Digital 101 web page
+        * Author: Joaquin Santiago Ruiz
+        * Date: 1.18.18
+
+         * Filename: ft.js
+ */
