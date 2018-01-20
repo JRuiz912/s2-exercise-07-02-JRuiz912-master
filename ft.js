fe1e082d797f0cc5428fe2ff2f9ec3871c8a3ce7
Joaquin Santiago <JRuiz912@west-mec.org>
2018-01-19 18:05:45 -0700

    Changes
    Today I added new pieces to code inside 3 different files. Inside the
    digital.html I added the l to html and the same thing for the estimate.
    Inside the ft.js set forms field to defaults.

--- ft.js
+++ ft.js
@@ -7,4 +7,20 @@
         * Date: 1.18.18
 
          * Filename: ft.js
+
+
+
  */
+
+
+ //set all forms field to defaults
+ function restForm() {
+   document.getElementById("photognum").value = 1;
+   document.getElementById("photoghrs").value = 2;
+   document.getElementById("membook") checked = false;
+   document.getElementById("reprodrights") checked = false;
+   document.getElementById("distance").value = 0;
+  
+
+//resets form when page is reloaded
+window.addEventListener("load", restForm, false);
