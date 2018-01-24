c604c4c2711186e6bd755d5970b382421c68019f
Joaquin Santiago <JRuiz912@west-mec.org>
2018-01-23 18:10:51 -0700

    Change
    Today I changes used operators to built expression and worked with
    arithmetic operation.  We use assignment operators

--- ft.js
+++ ft.js
@@ -17,6 +17,17 @@ var totalCost = 0;
 var memoryBook = false;
 var reproductionRight = false;
 
+//
+calculates all cost based on staff and adds to total cost
+function calcStaff(){
+  var num = documaent.getElementById("photognum");
+  document.getElementsById("photoghrs");
+  totalCost = photographerCost;
+  photographerCost = num.value *100* hrs.value;
+  totalCost += photographerCost
+  document.getelementById("estimate").innerHTML
+  = "$" + totalcode
+}
  //set all forms field to defaults
  function restForm() {
    document.getElementById("photognum").value = 1;
@@ -24,7 +35,12 @@ var reproductionRight = false;
    document.getElementById("membook") checked = memoryBook;
    document.getElementById("reprodrights") checked = reproductionRight;
    document.getElementById("distance").value = 0;
+   calcStaff()
     }
+  // created event listerner
+  function createEventListeners (){
+    document.getElementById("photognum").addevent
+  }
 
 //resets form when page is reloaded
 window.addEventListener("load", restForm, false);
